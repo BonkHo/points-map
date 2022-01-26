8fdfa418de96ab2ddac4062bfea5f8d14cf2ff91
John Bae <john21225@gmail.com>
2022-01-25 16:33:34 -0800

    Cities now show population based on size

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useWorldAtlas } from "./hooks/useWorldAtlas";
 import { useCities } from "./hooks/useCities";
+import { max, scaleSqrt } from "d3";
 import "./App.css";
 
 // Components
@@ -16,9 +17,21 @@ const App = () => {
 		return <pre>"Loading"</pre>;
 	}
 
+	const sizeValue = (d) => d.population;
+	const maxRadius = 15;
+
+	const sizeScale = scaleSqrt()
+		.domain([0, max(cities, sizeValue)])
+		.range([0, maxRadius]);
+
 	return (
 		<svg width={width} height={height}>
-			<Mark worldAtlas={worldAtlas} cities={cities} />
+			<Mark
+				worldAtlas={worldAtlas}
+				cities={cities}
+				sizeScale={sizeScale}
+				sizeValue={sizeValue}
+			/>
 		</svg>
 	);
 };
--- app/src/components/Mark.js
+++ app/src/components/Mark.js
@@ -5,7 +5,12 @@ const projection = geoNaturalEarth1();
 const path = geoPath(projection);
 const graticule = geoGraticule();
 
-const Mark = ({ worldAtlas: { land, interiors }, cities }) => {
+const Mark = ({
+	worldAtlas: { land, interiors },
+	cities,
+	sizeScale,
+	sizeValue,
+}) => {
 	return (
 		<g className="marks">
 			<path className="sphere" d={path({ type: "Sphere" })}></path>
@@ -16,7 +21,7 @@ const Mark = ({ worldAtlas: { land, interiors }, cities }) => {
 			<path className="interiors" d={path(interiors)}></path>
 			{cities.map((d) => {
 				const [x, y] = projection([d.lng, d.lat]);
-				return <circle cx={x} cy={y} r={1.5} />;
+				return <circle cx={x} cy={y} r={sizeScale(sizeValue(d))} />;
 			})}
 		</g>
 	);
